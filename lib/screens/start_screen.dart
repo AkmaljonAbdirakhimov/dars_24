b712433627884e7c35b5cbe7add6717296951595
Akmaljon Abdirakhimov <AkmaljonAbdirakhimov@gmail.com>
2024-04-29 20:05:22 +0500

    first commit

--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -1,27 +1,37 @@
+import 'package:dars_24/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:lottie/lottie.dart';
 
 class StartScreen extends StatelessWidget {
   // final String imageUrl = "https://pngimg.com/d/student_PNG62539.png";
   final String imageUrl =
-      "https://wallpapersprinted.com/download/2/nature_mountain_landscape_blue_lake-wallpaper-1920x1200.jpg";
+      "https://i.pinimg.com/originals/65/5d/2d/655d2da400a648abca600766a89deead.gif";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text("Asset image"),
-          Image.asset("assets/images/nature.jpeg"),
-          const SizedBox(height: 30),
-          const Text("Network image"),
-          Container(
-            color: Colors.red,
-            height: 400,
-            child: Image.network(
-              imageUrl,
-              fit: BoxFit.cover,
-              alignment: Alignment.centerLeft,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 60),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // SvgPicture.asset("assets/images/nature.svg"),
+              // Lottie.network(
+              //   "https://raw.githubusercontent.com/xvrh/lottie-flutter/master/example/assets/Mobilo/A.json",
+              // ),
+              // const Text("Asset image"),
+              // Image.asset("assets/images/nature.jpeg"),
+              // const SizedBox(height: 30),
+              // const Text("Network image"),
+              // Container(
+              //   color: Colors.red,
+              //   height: 300,
+              //   child: Image.network(
+              //     imageUrl,
+              //     fit: BoxFit.contain,
+              // alignment: Alignment.centerLeft,
               // color: Colors.blue,
               // colorBlendMode: BlendMode.color,
               // width: 400,
@@ -35,9 +45,83 @@ class StartScreen extends StatelessWidget {
               //     );
               //   }
               // },
-            ),
+              // ),
+              // ),
+              Container(
+                alignment: Alignment.bottomCenter,
+                width: double.infinity,
+                height: 500,
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(150),
+                    bottomRight: Radius.circular(150),
+                  ),
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Colors.orange.shade100,
+                      Colors.orange.shade300,
+                    ],
+                  ),
+                ),
+                child: Image.asset(
+                  "assets/images/image.png",
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(height: 70),
+              const Text(
+                "Xush kelibsiz bizning Flutter kursimizga!",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  height: 1,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 50),
+              InkWell(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (ctx) {
+                        return HomeScreen();
+                      },
+                    ),
+                  );
+                },
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.orange.shade200,
+                      width: 4,
+                    ),
+                    shape: BoxShape.circle,
+                  ),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.orange,
+                      shape: BoxShape.circle,
+                    ),
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "START",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
